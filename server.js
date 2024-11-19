e713f1d3ab30b2ec6f04c7628d1d057aca6f9650
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-18 17:57:40 -0700

    adding a get route

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Adds the middleware to parse the incoming request data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) => {
+    fs.readFile(.db/db.json, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    }
