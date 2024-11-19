518993d7d06c138b997630e71457590ca790f40c
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-18 18:18:25 -0700

    created routes for notes and added listener

--- server.js
+++ server.js
@@ -20,4 +20,53 @@ app.get('/', (req, res) => {
         } else {
             res.json(JSON.parse(data));
         }
-    }
+    });
+})
+
+// GET Route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// GET Route for all notes
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+});
+
+// POST Route for new notes
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    db.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(newNote);
+        }
+    });
+});         
+
+// DELETE Route for notes
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const noteIndex = db.findIndex(note => note.id === noteId);
+    db.splice(noteIndex, 1);
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(db);
+        }
+    });
+});
+
+// GET Route for all other pages
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
